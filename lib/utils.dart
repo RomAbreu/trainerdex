a330590b6b2f3656923880fef5179f1edd722b74
Romario Abreu <rias0331@gmail.com>
2024-11-02 16:52:21 -0400

    design: information containers for pokemons

--- lib/utils.dart
+++ lib/utils.dart
@@ -6,4 +6,10 @@ class Utils {
 
     return Color.lerp(color, Colors.white, amount)!;
   }
+
+  static Color darkenColor(Color color, [double amount = 0.85]) {
+    assert(amount >= 0 && amount <= 1, 'Amount must be between 0 and 1');
+
+    return Color.lerp(color, Colors.black, amount)!;
+  }
 }
