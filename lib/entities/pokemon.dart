6231ee2325f1240ab2b7f9e078dfe4565ef40b07
joseantt <joseant994@gmail.com>
2024-11-09 17:20:28 -0400

    feat: form name added to be more specific

--- lib/entities/pokemon.dart
+++ lib/entities/pokemon.dart
@@ -3,22 +3,32 @@ class Pokemon {
   final String name;
   final String spriteUrl;
   final List<String> types;
+  final List<String> formNames;
 
   Pokemon(
       {required this.id,
       required this.name,
       required this.spriteUrl,
-      required this.types});
+      required this.types,
+      required this.formNames});
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(
-      id: json['id'],
+      id: json['pokemon_species_id'],
       name: json['pokemon_v2_pokemonspecy']['pokemon_v2_pokemonspeciesnames'][0]
           ['name'],
       spriteUrl: json['pokemon_v2_pokemonsprites'][0]['sprites'],
       types: (json['pokemon_v2_pokemontypes'] as List)
           .map((type) => type['pokemon_v2_type']['name'] as String)
           .toList(),
+      formNames: (json['pokemon_v2_pokemonforms'][0]
+                  ['pokemon_v2_pokemonformnames']
+              .isEmpty)
+          ? []
+          : (json['pokemon_v2_pokemonforms'][0]['pokemon_v2_pokemonformnames']
+                  as List)
+              .map((form) => form['name'] as String)
+              .toList(),
     );
   }
 }
--- lib/repositories/pokemon_repository.dart
+++ lib/repositories/pokemon_repository.dart
@@ -2,10 +2,11 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:trainerdex/entities/pokemon.dart';
 
 class PokemonRepository {
-  static Future<List<Pokemon>> getAllPokemons(GraphQLClient client) async {
+  static Future<List<Pokemon>> getPokemonsWithOffset(
+      GraphQLClient client, int offset) async {
     const String query = """
-      query samplePokeAPIquery {
-        pokemon_v2_pokemon(limit: 25) {
+      query samplePokeAPIquery(\$offset: Int!) {
+        pokemon_v2_pokemon(offset: \$offset, limit: 12, order_by: {pokemon_species_id: asc}) {
           id
           pokemon_v2_pokemonsprites {
             sprites(path: "other.official-artwork.front_default")
@@ -15,17 +16,29 @@ class PokemonRepository {
               name
             }
           }
+          pokemon_v2_pokemonforms {
+            pokemon_v2_pokemonformnames(where: {_or: [
+              { pokemon_v2_language: { name: { _eq: "de" } } },
+              { pokemon_v2_language: { name: { _eq: "en" } } }
+              ]})
+            {
+              name
+            }
+          }
           pokemon_v2_pokemontypes {
             pokemon_v2_type {
               name
             }
           }
+          pokemon_species_id
         }
       }
     """;
 
     final QueryResult result =
-        await client.query(QueryOptions(document: gql(query)));
+        await client.query(QueryOptions(document: gql(query), variables: {
+      'offset': offset,
+    }));
 
     final List<dynamic> data = result.data?['pokemon_v2_pokemon'] ?? [];
     return data.map((json) => Pokemon.fromJson(json)).toList();
