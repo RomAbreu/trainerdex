dc1b639507706f29f86c7a60174fb9eaaa9c7230
Romario Abreu <rias0331@gmail.com>
2024-11-09 10:54:50 -0400

    feat: version group maap

--- lib/constants/app_values.dart
+++ lib/constants/app_values.dart
@@ -2,4 +2,28 @@ class AppValues {
   static const double kAppBarBackgroundLightenFactor = 0.3;
   static const double kPokemonDetailsImageHeight = 200;
   static const double kTextLightenFactor = 0.85;
+
+  static final Map<String, String> versionGroupMap = {
+    'red-blue': 'Red/Blue',
+    'yellow': 'Yellow',
+    'gold-silver': 'Gold/Silver',
+    'crystal': 'Crystal',
+    'ruby-sapphire': 'Ruby/Sapphire',
+    'emerald': 'Emerald',
+    'firered-leafgreen': 'FireRed/LeafGreen',
+    'diamond-pearl': 'Diamond/Pearl',
+    'platinum': 'Platinum',
+    'heartgold-soulsilver': 'HeartGold/SoulSilver',
+    'black-white': 'Black/White',
+    'black-2-white-2': 'Black 2/White 2',
+    'x-y': 'X/Y',
+    'omega-ruby-alpha-sapphire': 'Omega Ruby/Alpha Sapphire',
+    'sun-moon': 'Sun/Moon',
+    'ultra-sun-ultra-moon': 'Ultra Sun/Ultra Moon',
+    'letsgo-pikachu-eevee': 'Let\'s Go Pikachu/Eevee',
+    'sword-shield': 'Sword/Shield',
+    'brilliant-diamond-and-shining-pearl': 'Brilliant Diamond/Shining Pearl',
+    'legends-arceus': 'Legends: Arceus',
+    'scarlet-violet': 'Scarlet/Violet',
+  };
 }
