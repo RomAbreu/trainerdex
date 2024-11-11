a35f45e9c7a0f7c861ed7660d660288eba317b05
joseantt <joseant994@gmail.com>
2024-11-11 16:28:50 -0400

    feat: query changed to accept type filter

--- lib/repositories/pokemon_repository.dart
+++ lib/repositories/pokemon_repository.dart
@@ -3,10 +3,28 @@ import 'package:trainerdex/models/pokemon.dart';
 
 class PokemonRepository {
   static Future<List<Pokemon>> getPokemonsWithOffset(
-      GraphQLClient client, int offset) async {
-    const String query = """
+      GraphQLClient client, int offset,
+      [List<String>? typeFilter]) async {
+    final buffer = StringBuffer();
+
+    if (typeFilter != null && typeFilter.isNotEmpty) {
+      buffer.write(', where: { _or: [');
+
+      for (final type in typeFilter) {
+        buffer.write(
+            '{pokemon_v2_pokemontypes: {pokemon_v2_type: {name: {_eq: "$type"}}}},');
+      }
+
+      buffer.write(']}');
+    }
+
+    if (typeFilter != null && typeFilter.isEmpty) {
+      buffer.clear();
+    }
+
+    final String query = """
       query samplePokeAPIquery(\$offset: Int!) {
-        pokemon_v2_pokemon(offset: \$offset, limit: 12, order_by: {pokemon_species_id: asc}) {
+        pokemon_v2_pokemon(offset: \$offset, limit: 12, order_by: {pokemon_species_id: asc}${buffer.toString()}) {
           id
           pokemon_v2_pokemonsprites {
             sprites(path: "other.official-artwork.front_default")
