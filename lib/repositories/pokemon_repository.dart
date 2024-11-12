2a063ff8be83237d83c57f6dcccca6596acfa54c
joseantt <joseant994@gmail.com>
2024-11-11 20:09:06 -0400

    fix: to make filters more flexible

--- lib/repositories/pokemon_repository.dart
+++ lib/repositories/pokemon_repository.dart
@@ -4,27 +4,10 @@ import 'package:trainerdex/models/pokemon.dart';
 class PokemonRepository {
   static Future<List<Pokemon>> getPokemonsWithOffset(
       GraphQLClient client, int offset,
-      [List<String>? typeFilter]) async {
-    final buffer = StringBuffer();
-
-    if (typeFilter != null && typeFilter.isNotEmpty) {
-      buffer.write(', where: { _or: [');
-
-      for (final type in typeFilter) {
-        buffer.write(
-            '{pokemon_v2_pokemontypes: {pokemon_v2_type: {name: {_eq: "$type"}}}},');
-      }
-
-      buffer.write(']}');
-    }
-
-    if (typeFilter != null && typeFilter.isEmpty) {
-      buffer.clear();
-    }
-
-    final String query = """
-      query samplePokeAPIquery(\$offset: Int!) {
-        pokemon_v2_pokemon(offset: \$offset, limit: 12, order_by: {pokemon_species_id: asc}${buffer.toString()}) {
+      [List<String>? typeFilter, List<String>? generationFilter]) async {
+    const String query = """
+      query samplePokeAPIquery(\$offset: Int!, \$where: pokemon_v2_pokemon_bool_exp) {
+        pokemon_v2_pokemon(offset: \$offset, limit: 12, order_by: {pokemon_species_id: asc}, where: \$where) {
           id
           pokemon_v2_pokemonsprites {
             sprites(path: "other.official-artwork.front_default")
@@ -57,12 +40,30 @@ class PokemonRepository {
       }
     """;
 
+    final filters = _prepareFilters(typeFilter, generationFilter);
+
     final QueryResult result =
         await client.query(QueryOptions(document: gql(query), variables: {
       'offset': offset,
+      'where': filters.isEmpty ? {} : filters,
     }));
 
     final List<dynamic> data = result.data?['pokemon_v2_pokemon'] ?? [];
     return data.map((json) => Pokemon.fromJson(json)).toList();
+  }
+
+  static Map<String, dynamic> _prepareFilters(
+      [List<String>? typeFilter, List<String>? generationFilter]) {
+    final filters = <String, dynamic>{};
+
+    if (typeFilter != null && typeFilter.isNotEmpty) {
+      filters['pokemon_v2_pokemontypes'] = {
+        'pokemon_v2_type': {
+          'name': {'_in': typeFilter}
+        }
+      };
+    }
+
+    return filters;
   }
 }
