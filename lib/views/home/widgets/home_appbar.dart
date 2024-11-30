c3e11dad1923cbe659738c73b50cd90c67073692
joseantt <joseant994@gmail.com>
2024-11-30 01:24:40 -0400

    feat: appbar visuals updated for search and sort inputs

--- lib/views/home/widgets/home_appbar.dart
+++ lib/views/home/widgets/home_appbar.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:trainerdex/views/home/widgets/home_filter.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -26,11 +29,14 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text('TrainerDex'),
-      actions: <Widget>[
+      title: const TitleText(),
+      actions: [
         IconButton(
           onPressed: () {},
-          icon: Image.asset('assets/icon-pokeball-closed.png'),
+          icon: Image.asset(
+            'assets/icon-pokeball-closed.png',
+            width: 30,
+          ),
         ),
         IconButton(
           onPressed: () {
@@ -48,13 +54,118 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
                     ));
           },
           icon: const Icon(Icons.filter_alt_outlined),
-          iconSize: 35,
+          iconSize: 30,
         ),
         const SizedBox(width: 5),
       ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const SizedBox(width: 10),
+              const StyledSearchBar(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.sort),
+                iconSize: 30,
+              ),
+              const SizedBox(width: 5),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(100);
+}
+
+class TitleText extends StatelessWidget {
+  const TitleText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+          text: 'Trainer',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        const TextSpan(
+          text: 'Dex',
+          style: TextStyle(
+            color: Colors.black45,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class StyledSearchBar extends StatefulWidget {
+  const StyledSearchBar({super.key});
+
+  @override
+  State<StyledSearchBar> createState() => _StyledSearchBarState();
+}
+
+class _StyledSearchBarState extends State<StyledSearchBar> {
+  late StreamSubscription<bool> keyboardSubscription;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    var keyboardVisibilityController = KeyboardVisibilityController();
+
+    keyboardSubscription =
+        keyboardVisibilityController.onChange.listen((bool visible) {
+      if (!visible) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    keyboardSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final decoration = InputDecoration(
+      contentPadding: const EdgeInsets.only(top: 5),
+      hintText: 'Search for a Pokémon',
+      prefixIcon: const Icon(Icons.search),
+      filled: true,
+      fillColor: Theme.of(context).primaryColor.withAlpha(50),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide.none,
+      ),
+    );
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 5, bottom: 5, right: 5),
+        child: SizedBox(
+          height: 40,
+          child: TextField(
+            style: const TextStyle(fontSize: 14),
+            decoration: decoration,
+          ),
+        ),
+      ),
+    );
+  }
 }
